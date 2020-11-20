3b081dfa9ffa97f81622d19ea6b08707b627362a
Cenk Atahan Özbek <atahanozbek@outlook.com.tr>
2020-11-21 00:11:04 +0300

    next phase

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.mediaplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> musics;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        musics = new ArrayList<>();
+        musics.add("Pentagram-Sonsuz");
+        musics.add("Mor ve Ötesi-Uyan");
+
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, musics);
+        listView = findViewById(R.id.listView);
+        listView.setAdapter(listAdapter);
     }
 }
